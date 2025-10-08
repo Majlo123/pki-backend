14803c984c5f109ac49982c15491e4462d98f16c
Mihajlo Bogdanovic <bogdanovicmihajlo68@gmail.com>
2025-10-08 06:13:06 +0200

    feat:log updated

--- pki-backend/src/main/java/com/pki/pki_backend/aop/LoggingAspect.java
+++ pki-backend/src/main/java/com/pki/pki_backend/aop/LoggingAspect.java
@@ -1,52 +1,62 @@
 package com.pki.pki_backend.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    // Definišemo "pointcut" - koje metode želimo da pratimo
-    // Ovde pratimo sve javne metode unutar CertificateService i UserService
-    private final String servicePointcut = "execution(public * com.pki.pki_backend.service.CertificateService.*(..)) || " +
-            "execution(public * com.pki.pki_backend.service.UserService.createCaUser(..))";
+    // Definišemo "pointcut" koji obuhvata sve metode u servisima i kontrolerima
+    @Pointcut("within(com.pki.pki_backend.service..*) || within(com.pki.pki_backend.controller..*)")
+    public void applicationPackagePointcut() {
+        // Metoda je prazna jer služi samo kao nosilac @Pointcut anotacije
+    }
 
-    @Before(servicePointcut)
+    // Izvršava se pre poziva metode
+    @Before("applicationPackagePointcut()")
     public void logBefore(JoinPoint joinPoint) {
-        String user = getUsername();
-        logger.info("User: '{}' is starting execution of method: '{}'", user, joinPoint.getSignature().getName());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String user = (authentication != null) ? authentication.getName() : "anonymous";
+
+        // AŽURIRANO: Koristimo getSimpleName() da dobijemo samo ime klase
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("User: '{}' is starting execution of method: '{}.{}'", user, className, methodName);
     }
 
-    @AfterReturning(pointcut = servicePointcut, returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        String user = getUsername();
-        logger.info("User: '{}' successfully executed method: '{}'", user, joinPoint.getSignature().getName());
+    // Izvršava se nakon uspešnog završetka metode
+    @AfterReturning("applicationPackagePointcut()")
+    public void logAfterReturning(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String user = (authentication != null) ? authentication.getName() : "anonymous";
+
+        // AŽURIRANO: Koristimo getSimpleName() da dobijemo samo ime klase
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        log.info("User: '{}' successfully executed method: '{}.{}'", user, className, methodName);
     }
 
-    @AfterThrowing(pointcut = servicePointcut, throwing = "exception")
-    public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        String user = getUsername();
-        logger.error("User: '{}' encountered an error in method: '{}'. Exception: {}",
-                user, joinPoint.getSignature().getName(), exception.getMessage());
-    }
+    // Izvršava se ako metoda baci izuzetak (exception)
+    @AfterThrowing(pointcut = "applicationPackagePointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String user = (authentication != null) ? authentication.getName() : "anonymous";
 
-    private String getUsername() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            return ((UserDetails) principal).getUsername();
-        } else {
-            return principal.toString(); // Vraća npr. "anonymousUser" ako niko nije prijavljen
-        }
+        // AŽURIRANO: Koristimo getSimpleName() i izbacujemo stack trace, logujemo samo poruku greške
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+
+        log.error("User: '{}' encountered an error in method: '{}.{}' | Error: {}", user, className, methodName, e.getMessage());
     }
 }
+
